bb9911c7c27a45d9648f52517b7645fbba23a324
Miguel Casas <miguel@kommit.co>
2016-04-19 22:45:10 -0500

    add spinner

--- js/main.js
+++ js/main.js
@@ -2,9 +2,16 @@ $('#tracks-container').perfectScrollbar();
 var source   = $("#track-template").html();
 var template = Handlebars.compile(source);
 
+function spinner(){
+  return '<div class="brand_water">\
+           <i class="fa fa-spinner fa-spin fa-3x fa-fw margin-bottom"></i> \
+          </div>';
+}
+
 $('#input-finder').keyup(function(event) {
   if ( event.which == 13 && !!$.trim(this.value).length) {
     var text=$('#input-finder').val();
+    $('#tracks-container').html(spinner());
     $.ajax({
       url: 'https://api.spotify.com/v1/search?q='+text+'&type=track',
       type: 'GET',
