dfa7b0d7264c35116f5498cc2bf1bd6c0ffe9d69
Miguel Casas <miguel@kommit.co>
2016-04-19 22:21:32 -0500

    add validations input

--- js/main.js
+++ js/main.js
@@ -3,26 +3,29 @@ var source   = $("#track-template").html();
 var template = Handlebars.compile(source);
 
 $('#input-finder').keyup(function(event) {
-  if ( event.which == 13 ) {
-      var text=$('#input-finder').val();
-
-      $.ajax({
-        url: 'https://api.spotify.com/v1/search?q='+text+'&type=track',
-        type: 'GET',
-        dataType: 'json',
-      })
-      .done(function(data) {
-        console.log({'track':data.tracks.items});
-      var html= template({'track':data.tracks.items});
-      $('#tracks-container').html(html);
+  if ( event.which == 13 && !!$.trim(this.value).length) {
+    var text=$('#input-finder').val();
+    $.ajax({
+      url: 'https://api.spotify.com/v1/search?q='+text+'&type=track',
+      type: 'GET',
+      dataType: 'json',
+    })
+    .done(function(data) {
+        if(data.tracks.items.length===0){
+          $('#tracks-container').html('<div class="brand_water">Track, Not Found</div>');
+        }else{
+          var html= template({'track':data.tracks.items});
+          $('#tracks-container').html(html);
+        }
       $('#input-finder').val('');
-      })
-      .fail(function() {
-        console.log("error");
-      })
-      .always(function() {
-        console.log("complete");
-      });
-
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+  }else{
+    $('#tracks-container').html('<div class="brand_water">Please, input the name</div>');
   }
 });
